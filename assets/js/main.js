45cbb7196ae3391e72525ad80528b645a53641c8
motpuse-naveen <89586488+motpuse-naveen@users.noreply.github.com>
2021-10-27 18:27:02 +0530

    Copy new structure from Observing Night Sky

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,371 +1,87 @@
-var btnClicked;
-var hoverBtn;
-var section2Btn;
-var section3Btn;
-var section4Btn;
-var lastFocusedElement;
-
-$(document).on('click keydown', '.BtnText1', function (event) {
-    event.preventDefault();
-    if(event.type === 'click' || (event.type === 'keydown') && (event.keyCode === 13 || event.keyCode === 32)){
-        $(".Slides").css("display","none");
-        $(".BtnText1").removeClass("active")
-        $(this).addClass("active")
-        $(".BtnText1").removeClass("pointers")
-        $(this).addClass("pointers")
-        btnClicked = $(this).attr("slideNumber");
-        $(".slides"+ btnClicked).css("display","block");
-    }
-    if(event.type === 'keydown' && event.keyCode == 37){
-        $(this).parent('div').prev().find('.BtnText1').focus();
-    }
-    if(event.type === 'keydown' && event.keyCode == 39){
-        $(this).parent('div').next().find('.BtnText1').focus();
-    }
-    if(event.type === 'keydown' && event.shiftKey && event.keyCode == 9){
-        $('.contentContainer').find('[tabindex]:visible').last().focus();
-    }
-    else if(event.type === 'keydown' && (event.keyCode === 9|| event.keyCode === 40)){
-        $('.contentContainer').find('[tabindex]:visible').first().focus();
-    }
-});
-
-$(document).on('click keydown', '.section2Btns', function (event) {
-    event.preventDefault();
-    if(event.type === 'click' || (event.type === 'keydown') && (event.keyCode === 13 || event.keyCode === 32)){
-    	$(".section2Btns").removeClass("slide2activebtn")
-        $(this).addClass("slide2activebtn")
-        $(".section2Btns").removeClass("pointers")
-        $(this).addClass("pointers")
-        $('.section2insideSlides').css("display", "none");
-        section2Btn = $(this).attr("section2Buttons");
-        $(".s2Slide" + section2Btn).css("display", "block");
-    	 if(section2Btn==3){
-            $(".section2ActivitySupport").css("display", "block");
-        }else{
-    		$(".section2ActivitySupport").css("display", "none");
-    	}
-        // $(".s2Slide" + section2Btn).attr('tabindex',-1).focus();
-    }
-    if(event.type === 'keydown' && event.keyCode == 38){
-        $(this).prev().focus();
-    }
-    if(event.type === 'keydown' && event.keyCode == 40){
-        $(this).next().focus();
-    }
-    if(event.type === 'keydown' && event.shiftKey && event.keyCode == 9){
-        $('.topMenuBar2').find('[tabindex]:visible').last().focus();
-    }
-    else if(event.type === 'keydown' && event.keyCode === 9){
-        if($(".section2ActivitySupport").is(':visible')){
-            $(".section2ActivitySupport").focus();
-        }
-        else if($(".section2insideSlides").find('[tabindex]:visible').length > 0){
-            $(".section2insideSlides").find('[tabindex]:visible').first().focus();
-        }
-        else if($(".section2insideSlides").find('[tabindex]:visible').length == 0){
-            $(".section2insideSlides:visible").attr('tabindex','-1').focus();
-        }
-        else{
-            $('.mainContainer').find('[tabindex]:visible').first().focus();
-        }
-    }
-});
-
-$(document).on('click keydown', '.agendaText2', function (event) {
-    event.preventDefault();
-    if(event.type === 'click' || (event.type === 'keydown') && (event.keyCode === 13 || event.keyCode === 32)){
-    	$(".agendaText2").removeClass("slide3activebtn")
-        $(this).addClass("slide3activebtn")
-        $(".agendaText2").removeClass("pointers")
-        $(this).addClass("pointers")
-        $('.section3ContentContainer').css("display", "none");
-        section3Btn = $(this).attr("section3Buttons");
-        $(".s3Slide" + section3Btn).css("display", "block");
-        // $(".s3Slide" + section3Btn).focus();
-    }
-    if(event.type === 'keydown' && event.keyCode == 38){
-        $(this).prev().focus();
-    }
-    if(event.type === 'keydown' && event.keyCode == 40){
-        $(this).next().focus();
-    }
-    if(event.type === 'keydown' && event.shiftKey && event.keyCode == 9){
-        $('.topMenuBar2').find('[tabindex]:visible').last().focus();
-    }
-    else if(event.type === 'keydown' && event.keyCode === 9){
-        $(".disclaimerTxt1").find('[tabindex]:visible').first().focus();
-    }
-});
-
-$(document).on('click keydown', '.agendaText3', function (event) {
-    event.preventDefault();
-    if(event.type === 'click' || (event.type === 'keydown') && (event.keyCode === 13 || event.keyCode === 32)){
-    	$(".agendaText3").removeClass("slide4activebtn")
-        $(this).addClass("slide4activebtn")
-        $(".agendaText3").removeClass("pointers")
-        $(this).addClass("pointers")
-        $('.section4Container').css("display", "none");
-        section4Btn = $(this).attr("slide4Buttons");
-        $(".s4Slides" + section4Btn).css("display", "block");
-        $(".s4Slides" + section4Btn).focus();
-    }
-    if(event.type === 'keydown' && event.keyCode == 38){
-        $(this).prev().focus();
-    }
-    if(event.type === 'keydown' && event.keyCode == 40){
-        $(this).next().focus();
-    }
-    if(event.type === 'keydown' && event.shiftKey && event.keyCode == 9){
-        $('.topMenuBar2').find('[tabindex]:visible').last().focus();
-    }
-    else if(event.type === 'keydown' && event.keyCode === 9){
-        if($(".section4Container").find('[tabindex]:visible').length>0){
-            $(".section4Container").find('[tabindex]:visible').first().focus();
-        }
-        else{
-            $('.mainContainer').find('[tabindex]:visible').first().focus();
-        }
-    }
-});
-
-$(document).on('click keydown', '#closeIcon', function (event) {
-    if(event.type === 'click' || (event.type === 'keydown') && (event.keyCode === 13 || event.keyCode === 32 || event.keyCode === 27)){
-        event.preventDefault();
-        $('.section2Disclaimer').css("display", "none");
-        setTabIndex();
-        lastFocusedElement.focus();
-    }
-});
-
-$(document).on('click keydown', '.addimg', function (event) {
-    if(event.type === 'click' || (event.type === 'keydown') && (event.keyCode === 13 || event.keyCode === 32)){
-        event.preventDefault();
-	   // $('.Slides').css("display", "none");
-        $('.section8Slide1Div').css("display", "block");
-        removeTabIndex();
-        setCustomeTabIndex($('.section8Slide1Div'));
-        lastFocusedElement = $(this);
-    }
-});
-$(document).on('click keydown', '.colorBlue', function (event) {
-    if(event.type === 'click' || (event.type === 'keydown') && (event.keyCode === 13 || event.keyCode === 32)){
-        event.preventDefault();
-        $('.section2Disclaimer').css("display", "block");
-        removeTabIndex();
-        setCustomeTabIndex($('.section2Disclaimer'));
-        lastFocusedElement = $(this);
-    }
-});
-
-$(document).on('click keydown', '#slide1closeIcon1', function (event) {
-    if(event.type === 'click' || (event.type === 'keydown') && (event.keyCode === 13 || event.keyCode === 32 || event.keyCode === 27)){
-        event.preventDefault();
-        $('.section8Slide1Div').css("display", "none");
-        setTabIndex();
-        lastFocusedElement.focus();
-    }
-});
-$(document).on('click keydown', '.section2ActivitySupport', function (event) {
-    if(event.type === 'click' || (event.type === 'keydown') && (event.keyCode === 13 || event.keyCode === 32)){
-        event.preventDefault();
-        $('.section2ActivitySupportDiv').css("display", "block");
-        removeTabIndex();
-        setCustomeTabIndex($('.section2ActivitySupportDiv'));
-        lastFocusedElement = $(this);
-    }
-});
-$(document).on('click keydown', '#actcloseIcon', function (event) {
-    if(event.type === 'click' || (event.type === 'keydown') && (event.keyCode === 13 || event.keyCode === 32 || event.keyCode === 27)){
-        event.preventDefault();
-        $('.section2ActivitySupportDiv').css("display", "none");
-        setTabIndex();
-        lastFocusedElement.focus();
-    }
-});
-$(document).on('click keydown', '.section3Slide1ActSuppport', function (event) {
-    if(event.type === 'click' || (event.type === 'keydown') && (event.keyCode === 13 || event.keyCode === 32)){
-        event.preventDefault();
-        $('.section3Slide1ActSuppportDiv').css("display", "block");
-        removeTabIndex();
-        setCustomeTabIndex($('.section3Slide1ActSuppportDiv'));
-        lastFocusedElement = $(this);
-    }
-});
-
-$(document).on('click keydown', '#section3actcloseIcon', function (event) {
-    if(event.type === 'click' || (event.type === 'keydown') && (event.keyCode === 13 || event.keyCode === 32 || event.keyCode === 27)){
-        event.preventDefault();
-        $('.section3Slide1ActSuppportDiv').css("display", "none");
-        setTabIndex();
-        lastFocusedElement.focus();
-    }
-});
-$(document).on('click keydown', '.section3Slide2ActSupport1', function (event) {
-    if(event.type === 'click' || (event.type === 'keydown') && (event.keyCode === 13 || event.keyCode === 32)){
-        event.preventDefault();
-        $('.section4Slide2ActSupport1Div').css("display", "block");
-        removeTabIndex();
-        setCustomeTabIndex($('.section4Slide2ActSupport1Div'));
-        lastFocusedElement = $(this);
-    }
-});
-
-$(document).on('click keydown', '#section4actcloseIcon', function (event) {
-    if(event.type === 'click' || (event.type === 'keydown') && (event.keyCode === 13 || event.keyCode === 32 || event.keyCode === 27)){
-        event.preventDefault();
-        $('.section4Slide2ActSupport1Div').css("display", "none");
-        setTabIndex();
-        lastFocusedElement.focus();
-    }
-});
-
-$(document).on('click keydown', '.section3Slide2ActSupport2', function (event) {
-    if(event.type === 'click' || (event.type === 'keydown') && (event.keyCode === 13 || event.keyCode === 32)){
-        event.preventDefault();
-        $('.section4Slide2ActSupport2Div').css("display", "block");
-        removeTabIndex();
-        setCustomeTabIndex($('.section4Slide2ActSupport2Div'));
-        lastFocusedElement = $(this);
-    }
-});
-
-$(document).on('click keydown', '#section4actcloseIcon1', function (event) {
-    if(event.type === 'click' || (event.type === 'keydown') && (event.keyCode === 13 || event.keyCode === 32 || event.keyCode === 27)){
-        event.preventDefault();
-        $('.section4Slide2ActSupport2Div').css("display", "none");
-        setTabIndex();
-        lastFocusedElement.focus();
-    }
-});
-
-$(document).on('click keydown', '.section5Slide1ActSupport', function (event) {
-    if(event.type === 'click' || (event.type === 'keydown') && (event.keyCode === 13 || event.keyCode === 32)){
-        event.preventDefault();
-        $('.section5Slide1ActSupportDiv').css("display", "block");
-        removeTabIndex();
-        setCustomeTabIndex($('.section5Slide1ActSupportDiv'));
-        lastFocusedElement = $(this);
-    }
-});
-
-$(document).on('click keydown', '#section5actcloseIcon1', function (event) {
-    if(event.type === 'click' || (event.type === 'keydown') && (event.keyCode === 13 || event.keyCode === 32 || event.keyCode === 27)){
-        event.preventDefault();
-        $('.section5Slide1ActSupportDiv').css("display", "none");
-        setTabIndex();
-        lastFocusedElement.focus();
-    }
-});
-$(document).on('click keydown', '.act1', function (event) {
-    if(event.type === 'click' || (event.type === 'keydown') && (event.keyCode === 13 || event.keyCode === 32)){
-        event.preventDefault();
-        $('.section6Slide1ProTempDiv').css("display", "block");
-        removeTabIndex();
-        setCustomeTabIndex($('.section6Slide1ProTempDiv'));
-        lastFocusedElement = $(this);
-    }
-});
-
-$(document).on('click keydown', '#proTempcloseIcon1', function (event) {
-    if(event.type === 'click' || (event.type === 'keydown') && (event.keyCode === 13 || event.keyCode === 32 || event.keyCode === 27)){
-        event.preventDefault();
-        $('.section6Slide1ProTempDiv').css("display", "none");
-        setTabIndex();
-        lastFocusedElement.focus();
-    }
-});
-
-$(document).on('click keydown', '.act2', function (event) {
-    if(event.type === 'click' || (event.type === 'keydown') && (event.keyCode === 13 || event.keyCode === 32)){
-        event.preventDefault();
-        $('.section7Slide1rubricProjectDiv').css("display", "block");
-        removeTabIndex();
-        setCustomeTabIndex($('.section7Slide1rubricProjectDiv'));
-        lastFocusedElement = $(this);
-    }
-});
-
-$(document).on('click keydown', '#proRubricscloseIcon1', function (event) {
-    if(event.type === 'click' || (event.type === 'keydown') && (event.keyCode === 13 || event.keyCode === 32 || event.keyCode === 27)){
-        event.preventDefault();
-        $('.section7Slide1rubricProjectDiv').css("display", "none");
-        setTabIndex();
-        lastFocusedElement.focus();
-    }
-});
-
 $(document).ready(function () {
-    addAccessibilityRolesAndTab();
-    setTabIndex();   
-    document.addEventListener('keydown', function(e) {
+    //addAccessibilityRolesAndTab();
+    //setTabIndex();
+    document.addEventListener('keydown', function (e) {
         var code = e.keyCode ? e.keyCode : e.which;
         if (e.keyCode === 9) {
             $('body').addClass('show-focus-outlines');
         }
     });
-    document.addEventListener('click', function(e) {
+    document.addEventListener('click', function (e) {
         $('body').removeClass('show-focus-outlines');
-    }); 
+    });
+    setTimeout(function(){
+        $(".annim1").removeClass("annim1")
+        $(".annim2").removeClass("annim2")
+        $(".annim3").removeClass("annim3")
+        $(".annim4").removeClass("annim4")
+        $(".annim4-1").removeClass("annim4-1")
+        $(".annim4-2").removeClass("annim4-2")
+        $(".annim4-3").removeClass("annim4-3")
+        $(".annim4-4").removeClass("annim4-4")
+        $(".annim4-5").removeClass("annim4-5")
+        $(".annim4-11").removeClass("annim4-11")
+        $(".annim4-22").removeClass("annim4-22")
+        $(".annim4-33").removeClass("annim4-33")
+        $(".annim4-44").removeClass("annim4-44")
+        $(".annim4-55").removeClass("annim4-55")
+        $(".annim4-111").removeClass("annim4-111")
+        $(".annim4-222").removeClass("annim4-222")
+        $(".annim4-333").removeClass("annim4-333")
+        $(".annim4-444").removeClass("annim4-444")
+        $(".annim4-555").removeClass("annim4-555")
+    },3000)
 });
-function addAccessibilityRolesAndTab(){
-    /* Main */
-    $('.conatiner').attr('role','main').removeAttr('tabindex');
-    
-    /* Navigation */
-    $('.topMenuBar2').attr('role','menubar');
-    $('.topMenuBar2 .BtnText1').attr('role','menuitem').addClass('tabindex');
-    $('.topMenuBar2 .BtnText1').parent('div').attr('role','none');
 
-    /* Left Side Menubar */
-    $('.Slides .agendaText').closest('div').attr('role','menubar');
-    $('.Slides .agendaText').addClass('tabindex').attr('role','menuitem');
+$(document).on('click', '.link-learmore', function (event) {
+    if (event.type === 'click' || (event.type === 'keydown') && (event.keyCode === 13 || event.keyCode === 32)) {
+        event.preventDefault();
+        $(".container-fs").HideElement();
+        $('.container-fs-popup.disclaimer').ShowElement();
+    }
+});
 
-    $('.conatiner img').attr('alt','Placeholder');
-    $('.slide1Img2').attr('role','img');
-    $('.slide1Img2').attr('alt','Placeholder');
-    $('.conatiner a').addClass('tabindex');
+$(document).on('click', '#disclaimerClose', function (event) {
+    if (event.type === 'click' || (event.type === 'keydown') && (event.keyCode === 13 || event.keyCode === 32)) {
+        event.preventDefault();
+        $(".container-fs").ShowElement();
+        $('.container-fs-popup.disclaimer').HideElement();
+        setTabIndex();
+        lastFocusedElement.focus();
+    }
+});
 
-    $('.addimg').addClass('tabindex').attr('role','button').attr('alt','More Info');
-    $('#slide1closeIcon1').addClass('tabindex').attr('role','button').attr('aria-label','More Info Close');
-    
-    $('.activitySupportDiv').addClass('tabindex').attr('role','button');
-    $('#actcloseIcon').addClass('tabindex').attr('role','button').attr('aria-label','Activity Support Close');
-    $('#section3actcloseIcon').addClass('tabindex').attr('role','button').attr('aria-label','Activity Support Close');
+$(document).on("click", '.link-tab', function(event){
+    //debugger;
+    if(!$(this).hasClass("active")){
+        var dataid = $(this).attr("data-tabid");
+        $(".link-tab.active").removeClass("active")
+        $(this).addClass("active")
+        $(".container-fs.slide").InactiveTabs();
+        $("#" + dataid).ActiveTabs();
+    }
+});
 
-    $('.activitySupportDiv1').addClass('tabindex').attr('role','button');
-    $('#proTempcloseIcon1').addClass('tabindex').attr('role','button').attr('aria-label','Project Support Close');
-    $('#proRubricscloseIcon1').addClass('tabindex').attr('role','button').attr('aria-label','Project Rubic Close');
+$(document).on("click", ".side-nav-circle ul li a", function(event){
+    $(".side-nav-circle ul li a").removeClass("active");
+    $(this).addClass("active");
+})
 
-    $('#section4actcloseIcon').addClass('tabindex').attr('role','button').attr('aria-label','Activity Support Close');
-    $('#section4actcloseIcon1').addClass('tabindex').attr('role','button').attr('aria-label','Activity Support Close');
-    $('#section5actcloseIcon1').addClass('tabindex').attr('role','button').attr('aria-label','Activity Support Close');
+$(document).on("click", ".card-body .card-link", function(event){
+    var tablinkid = $(this).attr("data-tablinkid");
+    $("#" + tablinkid).trigger( "click" );
+})
+$(document).on("click", "#linkNameTheFullMoon", function(event){
+    $(".container-fs").HideElement();
+    $('.container-fs-popup.fullmoon').ShowElement();
+})
 
-    $('.disclaimerTxt .colorBlue').addClass('tabindex').attr('role','link').attr('href','#');
-    $('.disclaimerTxt1 .colorBlue').addClass('tabindex').attr('role','link').attr('href','#');
-    $('#closeIcon').addClass('tabindex').attr('role','button').attr('aria-label','Disclaimer Close');
+$(document).on("click", "#close-fullmoonimage", function(event){
+    $(".container-fs").ShowElement();
+    $('.container-fs-popup.fullmoon').HideElement();
+})
+$(document).on("click", "#fullmoonClose", function(event){
+    $(".container-fs").ShowElement();
+    $('.container-fs-popup.fullmoon').HideElement();
+})
 
-
-
-    $('.headingRole').attr('role','heading');
-
-}
-function setTabIndex(){
-    var tabIndex = 0;
-    $('.tabindex').each(function(){
-        $(this).attr('tabindex', tabIndex);
-    });
-}
-function removeTabIndex(){
-    var tabIndex = -1;
-    $('.tabindex').each(function(){
-        $(this).attr('tabindex', tabIndex).removeAttr('tabindex');
-    });
-}
-function setCustomeTabIndex(_outerElement){
-    var tabIndex = 0;
-    _outerElement.find('.tabindex').each(function(){
-        $(this).attr('tabindex', tabIndex);
-    });
-    _outerElement.attr('tabindex',-1).focus().css({'outline':'none'});
-}
---
+++ assets/js/utility.js
@@ -0,0 +1,110 @@
+$.fn.extend({
+    Disable: function () {
+        return this.each(function () {
+            $(this).attr("disabled", "disabled").attr("aria-disabled", "true").addClass("disabled")
+        });
+    },
+    Enable: function () {
+        return this.each(function () {
+            $(this).removeAttr("disabled").removeAttr("aria-disabled").removeClass("disabled")
+        });
+    },
+    ShowElement: function () {
+        return this.each(function () {
+            $(this).removeAttr("aria-hidden").removeClass("disnone");
+        });
+    },
+    HideElement: function () {
+        return this.each(function () {
+            $(this).attr("aria-hidden", "true").addClass("disnone");
+        });
+    },
+    InactiveTabs: function () {
+        return this.each(function () {
+            $(this).removeClass("active").addClass("inactive").attr("aria-hidden", "true");
+        });
+    },
+    ActiveTabs: function () {
+        return this.each(function () {
+            $(this).addClass("active").removeClass("inactive").removeAttr("aria-hidden");
+        });
+    }
+});
+
+(function(document, history, location) {
+    var HISTORY_SUPPORT = !!(history && history.pushState);
+  
+    var anchorScrolls = {
+      ANCHOR_REGEX: /^#[^ ]+$/,
+      OFFSET_HEIGHT_PX: 95,
+  
+      /**
+       * Establish events, and fix initial scroll position if a hash is provided.
+       */
+      init: function() {
+        //this.scrollToCurrent();
+        $(window).on('hashchange', $.proxy(this, 'scrollToCurrent'));
+        $('body').on('click', 'a', $.proxy(this, 'delegateAnchors'));
+      },
+  
+      /**
+       * Return the offset amount to deduct from the normal scroll position.
+       * Modify as appropriate to allow for dynamic calculations
+       */
+      getFixedOffset: function() {
+        return this.OFFSET_HEIGHT_PX;
+      },
+  
+      /**
+       * If the provided href is an anchor which resolves to an element on the
+       * page, scroll to it.
+       * @param  {String} href
+       * @return {Boolean} - Was the href an anchor.
+       */
+      scrollIfAnchor: function(href, pushToHistory) {
+        var match, anchorOffset;
+  
+        if(!this.ANCHOR_REGEX.test(href)) {
+          return false;
+        }
+  
+        match = document.getElementById(href.slice(1));
+  
+        if(match) {
+          anchorOffset = $(match).offset().top - this.getFixedOffset();
+          $('html, body').animate({ scrollTop: anchorOffset});
+          $("a[refid]").removeClass("active");
+          $("a[refid='" + href.slice(1) + "']").addClass("active");
+          // Add the state to history as-per normal anchor links
+          if(HISTORY_SUPPORT && pushToHistory) {
+            history.pushState({}, document.title, location.pathname + href);
+          }
+        }
+  
+        return !!match;
+      },
+      
+      /**
+       * Attempt to scroll to the current location's hash.
+       */
+      scrollToCurrent: function(e) { 
+        if(this.scrollIfAnchor(window.location.hash) && e) {
+            e.preventDefault();
+        }
+      },
+  
+      /**
+       * If the click event's target was an anchor, fix the scroll position.
+       */
+      delegateAnchors: function(e) {
+        var elem = e.target;
+  
+        if(this.scrollIfAnchor(elem.getAttribute('href'), true)) {
+          e.preventDefault();
+        }
+      }
+    };
+  
+      $(document).ready($.proxy(anchorScrolls, 'init'));
+  })(window.document, window.history, window.location);
+  
