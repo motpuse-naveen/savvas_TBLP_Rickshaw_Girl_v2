e618397dc3043ebf83c2ad14806257d667f9ea6c
motpuse-naveen <89586488+motpuse-naveen@users.noreply.github.com>
2021-10-29 16:07:43 +0530

    Final Chnages

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,27 +10,16 @@ $(document).ready(function () {
     document.addEventListener('click', function (e) {
         $('body').removeClass('show-focus-outlines');
     });
+    
     setTimeout(function(){
         $(".annim1").removeClass("annim1")
         $(".annim2").removeClass("annim2")
         $(".annim3").removeClass("annim3")
         $(".annim4").removeClass("annim4")
-        $(".annim4-1").removeClass("annim4-1")
-        $(".annim4-2").removeClass("annim4-2")
-        $(".annim4-3").removeClass("annim4-3")
-        $(".annim4-4").removeClass("annim4-4")
-        $(".annim4-5").removeClass("annim4-5")
-        $(".annim4-11").removeClass("annim4-11")
-        $(".annim4-22").removeClass("annim4-22")
-        $(".annim4-33").removeClass("annim4-33")
-        $(".annim4-44").removeClass("annim4-44")
-        $(".annim4-55").removeClass("annim4-55")
-        $(".annim4-111").removeClass("annim4-111")
-        $(".annim4-222").removeClass("annim4-222")
-        $(".annim4-333").removeClass("annim4-333")
-        $(".annim4-444").removeClass("annim4-444")
-        $(".annim4-555").removeClass("annim4-555")
+        $(".annim4").removeClass("annim5")
+        $(".annim4").removeClass("annim6")
     },3000)
+    
 });
 
 $(document).on('click', '.link-learmore', function (event) {
